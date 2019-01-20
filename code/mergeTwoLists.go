d84151c5b86eec34757b0e929626587a7742fb64
cannotprogram <657253114@qq.com>
2019-01-20 16:03:10 +0800

    update code

--- code/mergeTwoLists.go
+++ code/mergeTwoLists.go
@@ -6,28 +6,35 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
 	list := new(ListNode)
 	tmp := list
-	for{
+	for {
 		if l1.Val < l2.Val {
 			tmp.Val = l1.Val
 			tmp.Next = new(ListNode)
 			tmp = tmp.Next
 			l1 = l1.Next
 			if l1 == nil {
-				list.Next = l2
+				tmp.Val = l2.Val
+				tmp.Next = l2.Next
 				return list
 			}
-		}else {
-			list.Val = l2.Val
+		} else {
+			tmp.Val = l2.Val
 			tmp.Next = new(ListNode)
 			tmp = tmp.Next
 			l2 = l2.Next
 			if l2 == nil {
-				list.Next = l1
+				tmp.Val = l1.Val
+				tmp.Next = l1.Next
 				return list
 			}
 		}
 	}
-	//return list
 }
